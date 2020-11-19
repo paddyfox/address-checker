92e794d56d24a09a171870ef164f559b7e67d929
Paddy Fox <patrick.fox@digital.homeoffice.gov.uk>
2020-11-19 16:46:42 +0000

    Refactoring

--- src/main/java/AddressChecker.java
+++ src/main/java/AddressChecker.java
@@ -2,22 +2,22 @@ public class AddressChecker {
 
     public boolean postcodeValidator(String postcode) {
         // Google Regex pattern for valid UK postcodes
-        String regex_pattern = "(GIR 0AA)|((([A-Z-[QVX]][0-9][0-9]?)|(([A-Z-[QVX]][A-Z-[IJZ]][0-9\u200C\u200B][0-9]?)|(([A-Z-[QVX\u200C\u200B]][0-9][A-HJKSTUW])|\u200C\u200B([A-Z-[QVX]][A-Z-[IJ\u200C\u200BZ]][0-9][ABEHMNPRVWX\u200C\u200BY]))))\\s?[0-9][A-Z-[\u200C\u200BCIKMOV]]{2})";
+        String regexPattern = "(GIR 0AA)|((([A-Z-[QVX]][0-9][0-9]?)|(([A-Z-[QVX]][A-Z-[IJZ]][0-9\u200C\u200B][0-9]?)|(([A-Z-[QVX\u200C\u200B]][0-9][A-HJKSTUW])|\u200C\u200B([A-Z-[QVX]][A-Z-[IJ\u200C\u200BZ]][0-9][ABEHMNPRVWX\u200C\u200BY]))))\\s?[0-9][A-Z-[\u200C\u200BCIKMOV]]{2})";
         // return boolean after attempting to match postcode to the above pattern
-        return postcode.matches(regex_pattern);
+        return postcode.matches(regexPattern);
     }
 
     public boolean streetNumberValidator(String streetNumber) {
         // Regex pattern for numbers
-        String regex_pattern = "[0-9]+";
+        String regexPattern = "[0-9]+";
         // return boolean after attempting to match street number to the above pattern
-        return streetNumber.matches(regex_pattern);
+        return streetNumber.matches(regexPattern);
     }
 
     public boolean streetNameValidator(String streetName) {
         // Regex pattern for alpha characters
-        String regex_pattern = "^[a-zA-Z]+$";
+        String regexPattern = "^[a-zA-Z]+$";
         // return boolean after attempting to match street name to the above pattern
-        return streetName.matches(regex_pattern);
+        return streetName.matches(regexPattern);
     }
 }
